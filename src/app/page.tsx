20bcfb97a5b1624e626d5965da7e9641bb3df5dc
vanei2002 <vanei.jesus016@gmail.com>
2023-09-11 23:10:03 -0300

    atualização de components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import './page.sass'
 import { Navegation } from '@/components/navegation/navegation'
 import { HomePage } from '@/components/home/home'
 import { SoftwareSkills } from '@/components/softwareSkills/softwareSkills'
+import { ProjectsGitHub } from '@/components/projects/projectsGitHub'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navegation />
       <HomePage />
       <SoftwareSkills />
+      <ProjectsGitHub/>
     </main>
   )
 }
---
+++ src/components/projects/projectsGitHub.tsx
@@ -0,0 +1,10 @@
+
+
+import './projectsgithub.sass'
+export const ProjectsGitHub = () =>{
+    return(
+        <section>
+            <h1>My Projects</h1>
+        </section>
+    )
+}
